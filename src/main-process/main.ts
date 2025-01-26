1cf0651b8b9b8176892e35a5f748f486d591cf12
Nahuel Uliassi Pirchio <uliassipirchio@gmail.com>
2025-01-26 14:22:56 -0300

    [Fix] Auto reload  render

--- src/main-process/main.ts
+++ src/main-process/main.ts
@@ -1,10 +1,16 @@
 import {app, BrowserWindow} from "electron"
-import path from "path"
+// import path from "path"
 
 app.on("ready", ()=>{
     const mainWindow = new BrowserWindow()
 
-    const mainWindowFilePath = path.join(app.getAppPath(), "/dist-render/index.html")
-    mainWindow.loadFile(mainWindowFilePath)
+    // dev mode
+    const mainWindowPath = 'http://localhost:5173/'
+    mainWindow.loadURL(mainWindowPath)
+
+    // production mode
+    // const mainWindowFilePath = path.join(app.getAppPath(), "/dist-render/index.html")
+    // mainWindow.loadFile(mainWindowFilePath)
+    
 })
 
